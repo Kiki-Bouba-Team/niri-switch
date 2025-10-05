d1faceb1accff0f871fb4b144dbcb63b663f9d39
Cezary Świtała <58264216+MusicFreak456@users.noreply.github.com>
2025-10-05 19:57:55 +0200

    Refactor: Move methods related to gtk list of windows to separate file (#13)
    * Move methods related to gtk list of windows to separate file
    
    * Fix linter warnings

--- src/daemon/gui/mod.rs
+++ src/daemon/gui/mod.rs
@@ -3,6 +3,7 @@ mod store;
 mod style;
 mod window_info;
 mod window_item;
+mod window_list;
 
 use super::dbus;
 use super::niri_socket::NiriSocket;
@@ -15,8 +16,6 @@ use std::{
     collections::{HashMap, HashSet},
     sync::{Arc, Mutex},
 };
-use window_info::WindowInfo;
-use window_item::WindowItem;
 
 /* Type aliases to make signatures more readable */
 type GlobalStoreRef = Arc<Mutex<store::GlobalStore>>;
@@ -24,83 +23,6 @@ type WindowWeakRef = glib::WeakRef<gtk4::ApplicationWindow>;
 
 const GTK4_APP_ID: &str = "org.kikibouba.NiriSwitch";
 const CLIENT_REQUEST_CAP: usize = 20;
-
-/// Creates a gtk widget factory for displaying window information.
-fn create_window_widget_factory() -> gtk4::SignalListItemFactory {
-    /* GTK factory is an object responsible for producing widgets and binding
-     * data from the model */
-    let factory = gtk4::SignalListItemFactory::new();
-
-    /* Upon setup signal, we create empty widget for each item in the model */
-    factory.connect_setup(move |_, item| {
-        let window_item = WindowItem::default();
-
-        item.downcast_ref::<gtk4::ListItem>()
-            .expect("Needs to be a ListItem")
-            .set_child(Some(&window_item));
-    });
-
-    /* Upon bind signal we fill widgets using data stored in the model */
-    factory.connect_bind(move |_, item| {
-        let item = item
-            .downcast_ref::<gtk4::ListItem>()
-            .expect("Needs to be a ListItem");
-
-        let window_info = item
-            .item()
-            .and_downcast::<WindowInfo>()
-            .expect("The item has to be a 'WindowInfo'");
-
-        let window_item = item
-            .child()
-            .and_downcast::<WindowItem>()
-            .expect("The child needs to be a 'WindowItem'");
-
-        window_item.set_window_info(window_info);
-    });
-
-    factory
-}
-
-/// Handle the window focus choice
-fn handle_window_chosen(list: &gtk4::ListView, position: u32, store: &GlobalStoreRef) {
-    let window_info = list
-        .model()
-        .expect("List view should have a model")
-        .item(position)
-        .and_downcast::<WindowInfo>()
-        .expect("Model item has to be a 'WindowInfo'");
-
-    /* Create async context and next spawn separate thread that will perform the
-     * blocking calls */
-    glib::spawn_future_local(clone!(
-        #[weak]
-        list,
-        #[strong]
-        store,
-        async move {
-            let window_id = window_info.id();
-
-            /* Move the chosen window to the front of the window list */
-            store.lock().unwrap().window_cache.move_to_front(&window_id);
-
-            /* Socket uses blocking calls, so we create a separete thread */
-            gio::spawn_blocking(move || {
-                let mut store = store.lock().unwrap();
-                store.niri_socket.change_focused_window(window_id);
-            })
-            .await
-            .expect("Blocking call must succeed");
-
-            /* Close the window after changing focus */
-            let window = list
-                .root()
-                .and_downcast::<gtk4::Window>()
-                .expect("Root widget has to be a 'Window'");
-            window.close()
-        }
-    ));
-}
 
 /// Handle key press events on the main window
 fn handle_key_pressed(key: gdk4::Key, window_ref: &WindowWeakRef) -> glib::Propagation {
@@ -113,23 +35,6 @@ fn handle_key_pressed(key: gdk4::Key, window_ref: &WindowWeakRef) -> glib::Propa
     glib::Propagation::Proceed
 }
 
-/// Given a niri Window description returns a WindowInfo GObject
-fn get_widow_info_for_niri_window(window: &niri_ipc::Window, store: &GlobalStoreRef) -> WindowInfo {
-    let store = store.lock().unwrap();
-    let app_id = window.app_id.clone().unwrap_or_default();
-
-    /* Try to get information about the app that coresponds to the window */
-    match store.app_database.get_app_info(&app_id) {
-        Some(app_info) => {
-            let icon = app_info
-                .icon
-                .map(|icon| gio::Icon::deserialize(&icon).unwrap());
-            WindowInfo::new(window.id, &app_info.display_name, icon)
-        }
-        None => WindowInfo::new(window.id, &app_id, None),
-    }
-}
-
 /// Updates the cached window list with new windows, and remove the old ones
 fn update_window_cache(windows: &[niri_ipc::Window], store: &GlobalStoreRef) {
     /* Create a set of current window ids */
@@ -138,24 +43,6 @@ fn update_window_cache(windows: &[niri_ipc::Window], store: &GlobalStoreRef) {
     let mut store = store.lock().unwrap();
     /* Update the cache with the new id set */
     store.window_cache.update_cache(current_id_set);
-}
-
-/// Select the next element in the list, wrap back to the begining if end reached
-fn advance_the_selection(list: &gtk4::ListView) {
-    let selection_model = list
-        .model()
-        .expect("ListView needs to have a model")
-        .downcast::<gtk4::SingleSelection>()
-        .expect("Needs to be a 'SingleSelection' type");
-    let list_store = selection_model
-        .model()
-        .and_downcast::<gio::ListStore>()
-        .expect("Needs to be a 'ListStore type");
-
-    let selected = selection_model.selected();
-    let new_selected = (selected + 1) % list_store.n_items();
-    list.scroll_to(new_selected, gtk4::ListScrollFlags::FOCUS, None);
-    list.scroll_to(new_selected, gtk4::ListScrollFlags::SELECT, None);
 }
 
 /// Put the windows in the cached positions
@@ -183,24 +70,12 @@ async fn handle_daemon_activated(list: &gtk4::ListView, store: &GlobalStoreRef) 
 
     /* If window is already shown, simply advance the selection */
     if window.is_visible() {
-        advance_the_selection(list);
+        window_list::advance_the_selection(list);
         return;
     }
-    /* Else relad the window list and present the window */
-
-    let selection_model = list
-        .model()
-        .expect("ListView needs to have a model")
-        .downcast::<gtk4::SingleSelection>()
-        .expect("Needs to be a 'SingleSelection' type");
-    let list_store = selection_model
-        .model()
-        .and_downcast::<gio::ListStore>()
-        .expect("Needs to be a 'ListStore type");
-
-    /* Reload the listed windows, state might have changes since the last time.
+    /* Else reload the listed windows, state might have changed since the last time.
      * This is also the initial filling of the list. */
-    list_store.remove_all();
+    window_list::clear_the_list(list);
 
     /* niri socket uses blocking calls, so it will be run on a separate thread */
     let store_ref = store.clone();
@@ -228,11 +103,7 @@ async fn handle_daemon_activated(list: &gtk4::ListView, store: &GlobalStoreRef) 
     }
 
     /* Append windows to the list model */
-    for window in &windows {
-        /* Try to get information about the app that coresponds to the window */
-        let window_info = get_widow_info_for_niri_window(window, store);
-        list_store.append(&window_info);
-    }
+    window_list::fill_the_list(list, &windows, store);
 
     /* Next bring the window back to visibility */
     window.present();
@@ -251,26 +122,8 @@ async fn handle_dbus_event(event: dbus::DbusEvent, list: &gtk4::ListView, store:
 
 /// Creates the main window, widgets, models and factories
 fn activate(application: &gtk4::Application, global_store: &GlobalStoreRef) {
-    let window_store = gio::ListStore::new::<WindowInfo>();
-    let selection_model = gtk4::SingleSelection::new(Some(window_store));
-    let widget_factory = create_window_widget_factory();
-
-    let list_view = gtk4::ListView::builder()
-        .model(&selection_model)
-        .factory(&widget_factory)
-        .orientation(gtk4::Orientation::Horizontal)
-        .single_click_activate(true)
-        .css_name("window-list")
-        .build();
-
-    /* clone! macro will create another reference to socket object, so it can be moved
-     * to the closure. The closure can outlive ther current function scope, so it has hold
-     * own reference. */
-    list_view.connect_activate(clone!(
-        #[strong]
-        global_store,
-        move |list, position| handle_window_chosen(list, position, &global_store)
-    ));
+    /* Create widget for displaying windows */
+    let list_view = window_list::create_window_list(global_store);
 
     /* Create main window */
     let window = gtk4::ApplicationWindow::builder()
