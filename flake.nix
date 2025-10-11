7ac7f9469f4a6b527a4c1b4849ddd6cf427a8a2d
Zhaith Izaliel <zhaith.izaliel@ethereal-edelweiss.cloud>
2025-10-11 13:33:33 +0200

    build(nix): remove darwin systems, add another linux x64 system

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
   in
     flake-parts.lib.mkFlake {inherit inputs;} ({...}: {
-      systems = ["x86_64-linux" "aarch64-darwin" "x86_64-darwin"];
+      systems = ["x86_64-linux" "aarch64-linux"];
 
       perSystem = {pkgs, ...}: {
         devShells = {
